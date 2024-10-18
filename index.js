df9c05285586c092ba5fe7dea0330dc94ac4d403
bibianeze <ezebibian4@gmail.com>
2024-10-18 16:24:11 +0100

    created an endpoint

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express()
+
+
+
+app.get("/api/hello-world", (req, res) => {
+  res.send("Hello, World!");
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>console.log(`listening on port ${port}`));
+
+
+
+
+
+
